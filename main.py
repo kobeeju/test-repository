aefbb2ef6fcc0ee2d2eceb697ab6ba65f52e0b83
kobeeju <kobeeju26@gmail.com>
2025-11-17 03:00:25 +0900

    코드 일부 수정

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import requests
+from random import randint
 
 BASE_URL = "http://www.randomnumberapi.com"
 url = f"{BASE_URL}/api/v1.0/random"
 
-params = {
-    'min': 0,
-    'max': 9,
-    'count': 3
-}
+# API로 대체
+# def generate_random_number(start, end, count):
+#     numbers = []
+#     for _ in range(count):
+#         numbers.append(randint(start, end))
 
-try:
-    res = requests.get(url, params=params)
-    data = res.json()
-
-    print(data)
-except Exception as e:
-    print(e)
-from random import randint
-
-
-def generate_random_number(start, end, count):
-    numbers = []
-    for _ in range(count):
-        numbers.append(randint(start, end))
-
-    return numbers
+#     return numbers
 
 
 if __name__ == "__main__":
     print("숫자의 범위를 지정해 주세요.")
     start = int(input("시작: "))
     end = int(input("끝: "))
-    
-    print(generate_random_number(start, end, count=3))
+
+    params = {"min": start, "max": end, "count": 3}
+
+    try:
+        res = requests.get(url, params=params)
+        data = res.json()
+
+        print(data)
+    except Exception as e:
+        print(e)
+    from random import randint
